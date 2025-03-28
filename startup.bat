91872c6e8db13718fc5896b1a4dcc0ae6dd15bbc
FredReal123 <fredreal123@hotmail.com>
2025-03-28 16:41:17 -0400

    Create startup.bat

---
+++ startup.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Define the source and destination paths
+set source="D:\a\super-potato\super-potato\wallpaper.bat"
+set destination="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\wallpaper.bat"
+
+:: Copy the file from the source to the destination
+copy %source% %destination%
+
+:: Check if the copy was successful
+if %errorlevel% == 0 (
+    echo File copied successfully!
+) else (
+    echo Failed to copy the file.
+)
