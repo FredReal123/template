399909a8cb55d0609fcbf6d61550dc5fa9987a5f
FredReal123 <fredreal123@hotmail.com>
2025-03-28 17:48:43 -0400

    Update wallpaper.bat

--- wallpaper.bat
+++ wallpaper.bat
@@ -1,15 +1,26 @@
 @echo off
-:: Reset wallpaper to default (no wallpaper)
-reg add "HKEY_USERS\.DEFAULT\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
+:: Loop 20 times
+for /L %%i in (1,1,10) do (
+    :: Reset wallpaper to default (no wallpaper)
+    reg add "HKEY_USERS\.DEFAULT\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
 
-:: Reset wallpaper for current user (this will be empty)
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
+    :: Reset wallpaper for current user (this will be empty)
+    reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallPaper /t REG_SZ /d "D:\a\super-potato\super-potato\wallpaper.jpg" /f
 
-:: Set the light theme for apps (turn on light theme)
-reg add "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_SZ /d "0" /f
+    :: Set the dark mode for apps (turn on dark mode)
+    reg add "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_SZ /d "0" /f
 
-:: Set the light theme for current user (turn on light theme)
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_SZ /d "0" /f
+    :: Set the dark mode for current user (turn on dark mode)
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_SZ /d "0" /f
 
-:: Update the system parameters to apply the changes
-RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+    :: Set the dark mode for apps (turn on dark mode)
+    reg add "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_SZ /d "0" /f
+
+    :: Set the dark mode for current user (turn on dark mode)
+    reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_SZ /d "0" /f
+
+    :: Update the system parameters to apply the changes
+    RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+)
+
+echo dark mode enabled
